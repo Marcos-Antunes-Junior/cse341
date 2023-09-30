d325068d31448f5729b9fdc969ed958bb4b4508c
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-09-30 12:07:09 -0300

    "Finished assignment!"

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -20,7 +20,38 @@ const getDataById = async (req, res, next) => {
   })  
 }
 
+const createNewContact = async (req, res, next) => {
+const create = await mongodb.getDb().db().collection('contacts').insertOne(req.body)
+ if(create.acknowledged){
+  res.status(201).json(create)
+ } else {
+  res.status(500).json(create.error || 'Some error ocurred while creating the contact')
+ }
+}
+
+const updateContact = async (req, res, next) => {
+  const getId = req.params.id
+  const id = new ObjectId(getId)
+  const update = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: id }, req.body)
+  console.log(update)
+  if(update.modifiedCount > 0) {
+    res.status(201).send()
+  } else {
+    res.status(500).json(update.error || 'Some error ocurred while updating the contact')
+  }
+}
+
+const deleteContact = async (req, res, next) => {
+  const getId = req.params.id
+  const id = new ObjectId(getId)
+  const delContact = await mongodb.getDb().db().collection('contacts').deleteOne({_id: id}) 
+  if(delContact){
+  res.status(200).send()
+  } else {
+  res.status(500).json(delContact.error || 'Some error ocurred while deleting the contact')
+  }
+}
 
 
 
-module.exports = {getData, getDataById};
+module.exports = {getData, getDataById, createNewContact, updateContact, deleteContact};
--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -8,4 +8,11 @@ router.get('/', contactsController.getData);
 
 router.get('/:id', contactsController.getDataById);
 
+router.post('/', contactsController.createNewContact)
+
+router.put('/:id', contactsController.updateContact)
+
+router.delete('/:id', contactsController.deleteContact)
+
+
 module.exports = router;
